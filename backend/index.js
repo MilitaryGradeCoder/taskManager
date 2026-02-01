c820f7d297b5cbee8c1545327068c1b858de5091
MilitaryGradeCoder <rahrocks123@gmail.com>
2026-02-01 21:17:56 +0530

    Added Schema/model and service layer functions

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,26 @@ const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const taskRoutes = require("./routes/task.route");
+
 
 dotenv.config()
 
 const app = express();
+
+//connect DB
 mongoose.connect(process.env.DB_URI).then(()=>{
     console.log("Mongo DB Connected Successfully");
 }).catch((error)=>{console.log("Error connecting Mongo DB", error)})
 
-
+// implement middlewares
 app.use(cors());
 app.use(express.json());
 
+//implement routes
+app.use("/tasks", taskRoutes);
+
+// start server
 app.listen(process.env.PORT, ()=>{
     console.log(`Backend listening on port ${process.env.PORT}`)
 })
