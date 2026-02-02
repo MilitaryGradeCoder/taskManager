fa388503d5ca7ebe4380200587e4d7f5c01493ec
MilitaryGradeCoder <rahrocks123@gmail.com>
2026-02-03 00:29:03 +0530

    deployed backend on render

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8082/tasks";
+const API_URL = "https://taskmanager-h819.onrender.com/tasks";
 
 export const fetchTasks = async () => {
   const response = await axios.get(API_URL);
